023249ffc2f792353ffe64808b12760a32ec2e3c
hjelmare <hjelmare@users.noreply.github.com>
2014-11-12 16:41:52 +0100

    updated h-file

--- MD_functions.h
+++ MD_functions.h
@@ -1,6 +1,8 @@
 #ifndef _MD_functions_h
 #define _MD_functions_h
 
+extern double GetInstantTemperature(double [][3], int, double); 
 extern double GetKineticEnergy(double [][3], double, int);
+extern double GetAlpha(double, double, double, double);
 
 #endif 
