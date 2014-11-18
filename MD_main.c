1e27f3fa4c0cffce1addfac440572b6de0347169
karlssonalbinka <karlsson.albin.ka@gmail.com>
2014-11-18 20:09:16 +0100

    i MD_main.c har jag skrivit så att vi beräknar phi vilket leder till att vi kan få s och i sin tur osäkerheten.

--- MD_main.c
+++ MD_main.c
@@ -17,13 +17,16 @@ int main()
 {
 	// REMEMBER: \m/ METAL UNITS \m/
 	// simulation settings
-	double totalTime = 5;
+	double totalTime = 10;
 	double timestep = 0.01;
+	double equilibrationTime = 5;
 		
 	// physical parameters
 	int dim = 3;		// there's a few functions that are hardcoded for dim=3, not sure how to fix...
 	int nCells = 4;
+	int correlationDistance = 50;	// this constant determines how sepperated the points should be in the corr.function (for temperature)
 	int nParticles = 4*pow(nCells,dim);
+	int equilibrationSteps = equilibrationTime/timestep;
 	double wantedTemp = 500+273;   // The temperature that we want the system to stabilize around.
 	double wantedPressure = 6.32420934 * 0.0000001;	// The pressure that we want the system to stabilize around.
 	wantedPressure = 5*0.00001;
@@ -37,6 +40,10 @@ int main()
 	double pos[nParticles][dim];
 	double vel[nParticles][dim];
 	double force[nParticles][dim];
+	double savedValuesT[correlationDistance];
+	double meanValuesT[correlationDistance];
+	double savedValuesP[correlationDistance];
+	double meanValuesP[correlationDistance];
 	double energy, potentialEnergy, kineticEnergy;
 	double siquare_root_of_alpha;
 	double alphaT = 1;
@@ -47,6 +54,11 @@ int main()
 	double curtAlphaP;
 	double virial;
 	double volume;
+	double temp;
+	double test=0;
+	double meanTemp, meanPressure, meanSquareTemp, meanSquarePressure, meanTemp_ik[correlationDistance-1], meanPressure_ik[correlationDistance-1];
+	double phiTemp[correlationDistance], phiPressure[correlationDistance], meanTempSquare, meanPressureSquare;
+	
 
 	// derived quantities
 	double nSteps = totalTime/timestep;
@@ -87,8 +99,9 @@ int main()
 	FILE *ptFile;
 	ptFile = fopen("pt.data","w");
 
-printf("lattpar innan loopen: %e \n", latticeParameter);
-	for (i=1;i<nSteps;i++) {
+
+
+	for (i=1;i<equilibrationSteps;i++) {
 		// Update velocities and positions
 		for (j=0; j<nParticles; j++) {
 			for (k = 0; k<dim; k++) {
@@ -130,7 +143,90 @@ printf("lattpar innan loopen: %e \n", latticeParameter);
 				pos[j][k] = pos[j][k] * curtAlphaP;
 			}
 		}
+	}
+
+	for (i=equilibrationSteps;i<nSteps;i++) {
+		// Update velocities and positions
+		for (j=0; j<nParticles; j++) {
+			for (k = 0; k<dim; k++) {
+				vel[j][k] = vel[j][k] + 0.5*timestep*force[j][k] / mass;
+				pos[j][k] = pos[j][k] + timestep*vel[j][k];
+			}
+		}
 		
+		// Calculate forces so that we can take the next half step
+		get_forces_AL(force, pos, nCells*latticeParameter, nParticles);
+
+		// Update velocities again
+		for (j=0; j<nParticles; j++) {
+			for (k=0; k<dim; k++) {
+				vel[j][k] = vel[j][k] + 0.5*timestep*force[j][k] / mass;
+			}
+		}
+
+		potentialEnergy = get_energy_AL(pos, nCells*latticeParameter, nParticles);
+		kineticEnergy = GetKineticEnergy(vel, mass, nParticles);
+		energy = potentialEnergy + kineticEnergy;
+
+		currentTemp = GetInstantTemperature(vel, nParticles, mass, dim);
+		volume = pow(nCells*latticeParameter, 3);
+		virial = get_virial_AL(pos, nCells*latticeParameter, nParticles);
+		currentPressure = GetPressure(currentTemp, volume, virial, nParticles);
+
+
+		meanTemp += currentTemp;
+		meanPressure += currentPressure;
+		meanSquareTemp += currentTemp * currentTemp;
+		meanSquarePressure += currentPressure * currentPressure;
+		//Saves temp and preassure values in order to calculate s.
+		if (i < equilibrationSteps + correlationDistance){
+		savedValuesT[i % correlationDistance] = currentTemp;
+		savedValuesP[i % correlationDistance] = currentPressure;
+		printf("\n i=%d \t sparat T = %e \n", i, savedValuesT[0]);		
+		}else{  
+			test += 1;
+			// updates the saved values.
+			for (j = 0; j<correlationDistance - 1; j++){	
+			savedValuesT[j] = savedValuesT[j+1];
+			savedValuesP[j] = savedValuesP[j+1];	
+			}
+			savedValuesT[correlationDistance - 1] = currentTemp;
+			savedValuesP[correlationDistance - 1] = currentPressure;
+			
+			// calculates and saves f_i*f_k
+			for (j = 1; j<correlationDistance; j++){
+			meanTemp_ik[j-1] += savedValuesT[0] * savedValuesT[j];
+			meanPressure_ik[j-1] += savedValuesP[0]* savedValuesP[j];
+			}
+		}
+	}
+
+meanTemp = meanTemp/(nSteps-equilibrationSteps);
+meanSquareTemp = meanSquareTemp/(nSteps-equilibrationSteps);
+meanPressure = meanPressure/(nSteps-equilibrationSteps);
+meanSquarePressure = meanSquarePressure/(nSteps-equilibrationSteps);
+
+temp = nSteps-correlationDistance-equilibrationSteps;
+//The last step to calculate the mean values of meanTemp_ik & meanPressure_ik.
+for(i = 0; i<correlationDistance; i++){
+	meanTemp_ik[i] = meanTemp_ik[i]/temp;
+	meanPressure_ik[i] = meanPressure_ik[i]/temp;
+}
+
+//Calculates phi
+meanTempSquare = meanTemp*meanTemp;
+meanPressureSquare = meanPressure*meanPressure;
+for(i = 0; i<correlationDistance; i++){
+	phiTemp[i] = (meanTemp_ik[i] - meanTempSquare)/(meanSquareTemp - meanTempSquare);
+	phiPressure[i] = (meanPressure_ik[i] - meanPressureSquare)/(meanSquarePressure - meanPressureSquare);
+
+	printf("k=%d   -->   phi T = %e   phi P = %e \n", i, phiTemp[i], phiPressure[i]); 
+}
+
+
+		// Savesthevalues needed to calculate the statistical inefficiency (s), for T and P.
+		
+/*		
 if(i % 5 == 0){
 	printf("it %d T %e \t alphaT %e \t P %e \t alphaP %e \n", i, currentTemp, alphaT, currentPressure, alphaP);
 }
@@ -140,7 +236,7 @@ if(i % 5 == 0){
 	}
 
 printf("lattpar efter loopen: %e \n", latticeParameter);
-
+*/
 
     /*
      Descriptions of the different functions in the files initfcc.c and alpotential.c are listed below.
@@ -175,5 +271,5 @@ printf("lattpar efter loopen: %e \n", latticeParameter);
     /*
      get_forces_AL(f,pos, L, N);
      */
->>>>>>> 5f87f93470cfda3cd81ecf11fe38acdcd47701cd
+// >>>>>>> 5f87f93470cfda3cd81ecf11fe38acdcd47701cd
 }
