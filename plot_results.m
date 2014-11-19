4e24ea8a7e2c13258de945be3cf8d1a3fe4c4d45
karlssonalbinka <karlsson.albin.ka@gmail.com>
2014-11-19 21:52:14 +0100

    bla

--- plot_results.m
+++ plot_results.m
@@ -10,9 +10,8 @@ kineticEnergy = data(:,4);
 
 data = importdata('pt.data');
 
-pressure = data(:,2);
-temp = data(:,3);
-
+temp = data(:,2);
+pressure = data(:,3);
 
 %%
 hold on
@@ -23,30 +22,19 @@ legend('total energy', 'potential energy', 'kinetic energy');
 ylabel('energy [eV]');
 xlabel('time (ps)');
 %%
-plot(data(:,1), pressure, 'c');
-plot(data(:,1), temp, 'k');
+hold on
+subplot(2,1,1);
+plot(data(:,1), pressure, 'g');
+hold on
+subplot(2,1,2);
+plot(data(:,1), temp, 'm');
 
-legend('total energy', 'potential energy', 'kinetic energy', 'pressure', 'temp');
+legend('pressure', 'temp');
 ylabel('energy [eV]');
 xlabel('time (ps)');
 
-%useFrom = 0.4;
-%start = fix(length(energy)*useFrom);
+useFrom = 0.6;
+start = fix(length(energy)*useFrom);
 
-<<<<<<< HEAD
-%meanTemp = mean(temp(start:end))
-%meanPress = mean(pressure(start:end))
-=======
 meanTemp = mean(temp(start:end))
 meanPress = mean(pressure(start:end))
-
-%%
-
-clear all
-clc
-clf
-
-data = importdata('velcor.data');
-
-plot(data(:,1),data(:,2))
->>>>>>> 2ea529a6a51b442c974fbdd948fd05dcd4568c6f
