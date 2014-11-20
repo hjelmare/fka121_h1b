2cf41ad3581e64f148adad7b497b919fc6607095
karlssonalbinka <karlsson.albin.ka@gmail.com>
2014-11-20 10:02:38 +0100

    fixat plottar och s-parametern

--- plotCalculatedPhi.m
+++ plotCalculatedPhi.m
@@ -8,10 +8,11 @@ phiT = importdata('phiTemp.data');
 plot(phiT(:,1), phiT(:,2), 'b');
 hold on
 plot(phiP(:,1), phiP(:,2), 'g');
-legend('phi för temp', 'phi för pressure');
 ylim([-1, 1])
-
+xlabel('time [ps]')
+ylabel('\phi (k)')
 plot(phiT(:,1), exp(-2),'r');
+legend('correlationfunction (\phi) - temperature', 'correlationfunction (\phi) - pressure', 'e^{-2}');
 
 %% Plot positions (3D-plot) of one particle
 clear all
@@ -20,4 +21,6 @@ pos = importdata('position.data');
 
 hold on
 plot3(pos(:,2), pos(:,3),pos(:,4),'g');
-
+xlabel('[Å]');
+ylabel('[Å]');
+zlabel('[Å]');
