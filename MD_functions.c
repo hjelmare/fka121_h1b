c9677ea12cba7a835b6ff6bfae60ba356312afee
karlssonalbinka <karlsson.albin.ka@gmail.com>
2014-11-21 12:09:10 +0100

    ffg

--- MD_functions.c
+++ MD_functions.c
@@ -2,44 +2,44 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define BOLTZMANN 8.6173324*0.00001	// in eV/K
+#define BOLTZMANN 8.6173324*0.00001 // in eV/K
 
 
 double getDistanceSquared(double vec1[3], double vec2[3])
 {
-	double sum = 0;
-	double temporary;
-	int i;
-	
+  double sum = 0;
+  double temporary;
+  int i;
+  
 
-	for(i = 0; i<3; i++){
-		temporary = vec1[i] - vec2[i];
-		sum += pow(temporary,2);
-	}
-	return sum;
+  for(i = 0; i<3; i++){
+    temporary = vec1[i] - vec2[i];
+    sum += pow(temporary,2);
+  }
+  return sum;
 }
 
 double ScalarProduct(double vec1[3], double vec2[3])
 {
-	double product = 0;
-	int i;
+  double product = 0;
+  int i;
 
-	for(i = 0 ; i < 3 ; i++) {
-		product += vec1[i]*vec2[i];
-	}
+  for(i = 0 ; i < 3 ; i++) {
+    product += vec1[i]*vec2[i];
+  }
 
-	return product;
+  return product;
 }
 
 
 double GetPressure(double temperature, double volume, double virial, \
 int nParticles)
 {
-	double pressure = 0;
+  double pressure = 0;
 
-	pressure = (nParticles * BOLTZMANN * temperature + virial)/volume;
+  pressure = (nParticles * BOLTZMANN * temperature + virial)/volume;
 
-	return pressure;
+  return pressure;
 }
 
 double GetInstantTemperature(double vel [][3], int nParticles, \
@@ -47,57 +47,58 @@ double mass, int dim)
 
 
 {
-	double temperature = 0;
-	int i,j;
+  double temperature = 0;
+  int i,j;
 
-	double factor = 2/(3*nParticles*BOLTZMANN);
+  double factor = 2/(3*nParticles*BOLTZMANN);
 
-	for (i=0;i<nParticles;i++) {
-		for (j=0;j<dim;j++) {
-			temperature += factor * pow(vel[i][j],2)*mass/2;
-		}
-	}	
+  for (i=0;i<nParticles;i++) {
+    for (j=0;j<dim;j++) {
+      temperature += factor * pow(vel[i][j],2)*mass/2;
+    }
+  } 
 
-	return temperature;
+  return temperature;
 }
 
 // Calculates the kinetic energy
 double GetKineticEnergy(double vel[][3], double mass, int nParticles)
 {
-	int i,j;
-	double sum;
-	double energy = 0;
+  int i,j;
+  double sum;
+  double energy = 0;
 
-	for(i = 0; i<nParticles; i++) {
-		sum = 0;
-		for(j = 0; j<3; j++) {
-			sum +=vel[i][j]*vel[i][j];
-		}
-		energy += sum*mass/2;
-	}
-	return energy;
+  for(i = 0; i<nParticles; i++) {
+    sum = 0;
+    for(j = 0; j<3; j++) {
+      sum +=vel[i][j]*vel[i][j];
+    }
+    energy += sum*mass/2;
+  }
+  return energy;
 }
 
 // Calculate alphaT (the modifier to get the right temperature)
 double GetAlphaT(double wantedTemp, double currentTemp, \
-												double timestep, double timeConstant)
+                        double timestep, double timeConstant)
 {
-	double alpha;
-	alpha = 1 + timestep/timeConstant * (wantedTemp-currentTemp)/currentTemp;
+  double alpha;
+  alpha = 1 + timestep/timeConstant * (wantedTemp-currentTemp)/currentTemp;
 
-	return alpha;
+  return alpha;
 }
 
 
 
 // Calculate alphaP (the modifier to get the right pressure)
 double GetAlphaP(double wantedPressure, double currentPressure, \
-													double timestep, double timeConstant)
+                          double timestep, double timeConstant)
 {
-	double kappa = 2.21901454; // Å^3/eV (at 300K)
-	double alpha;
+  double kappa = 2.21901454; // angstrom^3/eV (at 300K)
+  double alpha;
 
-	alpha = 1 - kappa*timestep/timeConstant*(wantedPressure - currentPressure);
-	
-	return alpha;
+  alpha = 1 - kappa*timestep/timeConstant* \
+        (wantedPressure - currentPressure);
+  
+  return alpha;
 }
