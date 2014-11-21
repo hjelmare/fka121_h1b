ffaf99cbb1ffd384e5a7d73049ab7110647e07d1
karlssonalbinka <karlsson.albin.ka@gmail.com>
2014-11-21 09:15:05 +0100

    fixat matlab-bilder

--- plotCalculatedPhi.m
+++ plotCalculatedPhi.m
@@ -17,10 +17,21 @@ legend('correlationfunction (\phi) - temperature', 'correlationfunction (\phi) -
 %% Plot positions (3D-plot) of one particle
 clear all
 clc
-pos = importdata('position.data');
+pos1 = importdata('position1.data');
+pos2 = importdata('position2.data');
+pos3 = importdata('position3.data');
+%%
+
+textStorlek = 14;
+legendStorlek = 11;
 
 hold on
-plot3(pos(:,2), pos(:,3),pos(:,4),'g');
-xlabel('[Å]');
-ylabel('[Å]');
-zlabel('[Å]');
+plot3(pos1(:,2), pos1(:,3),pos1(:,4),'b');
+plot3(pos2(:,2), pos2(:,3),pos2(:,4),'g.');
+plot3(pos3(:,2), pos3(:,3),pos3(:,4),'r.-');
+text = legend('T=773K', 'T=973K', 'T=1173K');
+set(text, 'FontSize', legendStorlek);
+
+xlabel('[Å]', 'FontSize',textStorlek);
+ylabel('[Å]', 'FontSize',textStorlek);
+zlabel('[Å]', 'FontSize',textStorlek);
